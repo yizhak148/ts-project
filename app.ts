e678b128bce204d06eca6d85b2229cdab01c8faa
yizhak148 <yizhak208@gmail.com>
2023-11-23 18:30:35 +0200

    e

---
+++ app.ts
@@ -0,0 +1,59 @@
+console.log("conect");
+
+const Professions:string[] = [
+    "math", "language", "history", "Geography",
+]
+
+const Teachers:object[] = [
+    {
+        teachername: "gilad",
+        Professions: "math"
+    },
+    {
+        teachername: "noam",
+        Professions: "Geography"
+    },
+    {
+        teachername: "eden",
+        Professions: "history"
+    },
+]
+
+let students = [
+    {
+        studentname: "bob",
+        class:"th8"
+    },
+    {
+        studentname: "jhon",
+        class:"th2"
+    },
+    {
+        studentname: "jasica",
+        class:"th6"
+    },
+    {
+        studentname: "mike",
+        class:"th7"
+    },
+    {
+        studentname: "",
+        class:""
+    },
+    {
+        studentname: "",
+        class:""
+    },
+    {
+        studentname: "",
+        class:""
+    },
+    {
+        studentname: "",
+        class:""
+    },
+]
+
+function addstudent (name:string, classnum:string){
+
+}
---
+++ dist/app.js
@@ -0,0 +1,54 @@
+console.log("conect");
+var Professions = [
+    "math", "language", "history", "Geography",
+];
+var Teachers = [
+    {
+        teachername: "gilad",
+        Professions: "math"
+    },
+    {
+        teachername: "noam",
+        Professions: "Geography"
+    },
+    {
+        teachername: "eden",
+        Professions: "history"
+    },
+];
+var students = [
+    {
+        studentname: "bob",
+        "class": "th8"
+    },
+    {
+        studentname: "jhon",
+        "class": "th2"
+    },
+    {
+        studentname: "jasica",
+        "class": "th6"
+    },
+    {
+        studentname: "mike",
+        "class": "th7"
+    },
+    {
+        studentname: "",
+        "class": ""
+    },
+    {
+        studentname: "",
+        "class": ""
+    },
+    {
+        studentname: "",
+        "class": ""
+    },
+    {
+        studentname: "",
+        "class": ""
+    },
+];
+function addstudent(name, classnum) {
+}
